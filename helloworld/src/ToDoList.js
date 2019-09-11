2a8ef493ffa3a96036c9e62dc7efd571121a7bbe
Joshua Chen <joshua.chen@nzte.govt.nz>
2019-09-11 14:37:22 +1200

    Create ToDoList.js

---
+++ helloworld/src/ToDoList.js
@@ -0,0 +1,36 @@
+import React, {Component} from "react";
+
+class ToDoList extends Component
+{
+  constructor(props)
+  {
+    super(props)
+
+    this.state = {
+      items: []
+    }
+
+    this.addItem = this.addItem.bind(this);
+  }
+
+  addItem(e)
+  {
+
+  }
+
+  render() {
+      return (
+        <div className="todoListMain">
+          <div className="header">
+            <form onSubmit={this.addItem}>
+              <input  ref={(a)=>this._inputElement = a} 
+                      placeholder="enter task">
+              </input>
+              <button type="submit">add</button>
+            </form>
+          </div>
+        </div>
+      );
+  }
+}
+export default ToDoList;
