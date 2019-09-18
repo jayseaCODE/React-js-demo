b8f7913eee970b6527f35ac4aeac094da158bc78
Joshua Chen <jayseaLEARN@hotmail.com>
2019-09-18 17:27:26 +1200

    feat: basic Menu Container component

---
+++ helloworld/src/MenuContainer.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import './MenuContainer.css'
+
+class MenuContainer extends Component
+{
+    render()
+    {
+        return (
+            <div className="menuContainerMain">
+                <div>
+                <p>Can you spot the item that doesn't belong?</p>
+                <ul>
+                    <li>Lorem</li>
+                    <li>Ipsum</li>
+                    <li>Dolor</li>
+                    <li>Sit</li>
+                    <li>Bumblebees</li>
+                    <li>Aenean</li>
+                    <li>Consectetur</li>
+                </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MenuContainer;
