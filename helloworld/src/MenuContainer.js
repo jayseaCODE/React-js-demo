0413830de1cbcb68809739a9bd201a8107db88a5
Joshua Chen <jayseaLEARN@hotmail.com>
2019-09-19 11:55:55 +1200

    add state and method to toggle state

--- helloworld/src/MenuContainer.js
+++ helloworld/src/MenuContainer.js
@@ -1,8 +1,30 @@
 import React, {Component} from 'react';
 import './MenuContainer.css'
+import ToDoList from './ToDoList';
 
 class MenuContainer extends Component
 {
+    constructor(props)
+    {
+        super(props);
+
+        this.state = {
+            "visible" : false
+        };
+
+        this.toggleMenu = this.toggleMenu.bind(this);
+    }
+
+    toggleMenu()
+    {
+        this.setState((prevState) => 
+        {
+            this.state = {
+                "visible" : !prevState
+            }
+        })
+    }
+
     render()
     {
         return (
