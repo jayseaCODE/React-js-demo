33fc6085bf775b1ea091e27cb4fd24ef829c9949
Joshua Chen <jayseaLEARN@hotmail.com>
2019-09-26 08:30:23 +1200

    update MenuButton to block render call
    only when it is called to update itself, because there is no need for MenuButton

--- helloworld/src/MenuButton.js
+++ helloworld/src/MenuButton.js
@@ -2,6 +2,11 @@ import React, {Component} from 'react';
 import './MenuButton.css';
 
 class MenuButton extends Component{
+    // Lifecycle method, allowing us to block the call to render()
+    shouldComponentUpdate(nextProps, nextState){
+        return false;
+    }
+
     render(){
         return (
             <button id="menuButton"
