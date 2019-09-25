e66a8839d2424736f630488e20e3cd60a167ffe0
Joshua Chen <jayseaLEARN@hotmail.com>
2019-09-26 08:33:14 +1200

    refactor check if needed to render
    only if the known prop value is changed

--- helloworld/src/MenuButton.js
+++ helloworld/src/MenuButton.js
@@ -4,7 +4,13 @@ import './MenuButton.css';
 class MenuButton extends Component{
     // Lifecycle method, allowing us to block the call to render()
     shouldComponentUpdate(nextProps, nextState){
-        return false;
+        if (nextProps.handleMouseDown === this.props.handleMouseDown)
+        {
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 
     render(){
