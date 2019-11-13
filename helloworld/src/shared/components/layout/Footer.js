c4adcccd44209c5640055fee89e7adc61b628e45
Joshua Chen <jaysealearn@hotmail.com>
2019-11-13 18:58:35 +1300

    refactor: Footer from class component to functional component

--- helloworld/src/shared/components/layout/Footer.js
+++ helloworld/src/shared/components/layout/Footer.js
@@ -1,12 +1,9 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Footer extends Component {
-    render()
-    {
-        return (
-            <footer>&copy; JaySeaCode {(new Date()).getFullYear()}</footer>
-        )
-    }
+const Footer = () => {
+    return (
+        <footer>&copy; JaySeaCode {(new Date()).getFullYear()}</footer>
+    )
 }
 
 export default Footer;
