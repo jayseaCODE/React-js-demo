407227851fa884cf2051e0c1c9199fcba19d3bf9
Joshua Chen <jaysealearn@hotmail.com>
2019-11-13 18:58:42 +1300

    refactor: Content from class component to functional component

--- helloworld/src/shared/components/layout/Content.js
+++ helloworld/src/shared/components/layout/Content.js
@@ -1,16 +1,13 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Content extends Component {
-    render()
-    {
-        const { children } = this.props;
-        return (
-            <main>
-                {children}
-            </main>
-        )
-    }
+const Content = props => {
+    const { children } = props;
+    return (
+        <main>
+            {children}
+        </main>
+    )
 }
 
 Content.propTypes =
