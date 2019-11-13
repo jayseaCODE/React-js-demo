9f725cb52b623296c10b6a578430f8146f52c0c0
Joshua Chen <jaysealearn@hotmail.com>
2019-11-13 18:58:28 +1300

    refactor: Header from class component to functional component

--- helloworld/src/shared/components/layout/Header.js
+++ helloworld/src/shared/components/layout/Header.js
@@ -1,15 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Header extends Component {
-    render(){
-        const {title = 'Welcome to React'} = this.props;
-        return (
-            <header>
-                <h1>{title}</h1>
-            </header>
-        );
-    }
+// Header - a functional/stateless component, for rendering static markup
+const Header = props => {
+    const {title = 'Welcome to React'} = props;
+    return (
+        <header>
+            <h1>{title}</h1>
+        </header>
+    );
 }
 
 // Here you can define your PropTypes
