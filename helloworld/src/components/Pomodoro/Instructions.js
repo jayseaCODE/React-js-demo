41f2540e38b64b2234e7fc9beeb9512ac3974424
Joshua Chen <jaysealearn@hotmail.com>
2019-11-15 20:40:08 +1300

    refactor: Instructions with edited instructions

--- helloworld/src/components/Pomodoro/Instructions.js
+++ helloworld/src/components/Pomodoro/Instructions.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 
 const Instructions = () =>
 (
         <div className="Instructions">
-            <h1>Underlying principles</h1>
-            <p>There are six steps in the original Pomodoro technique:</p>
+            <h1>How to use</h1>
+            <p>Here are six steps:</p>
             <ol>
-                <li>Decide on the task to be done.</li>
-                <li>Set the Pomodoro timer (traditionally to 25 minutes).</li>
+                <li>In our <NavLink to="/todolist">To Do List</NavLink>, add a task for you to do.</li>
+                <li>Start the Pomodoro timer (traditionally to 25 minutes) by clicking on "Start Working".</li>
                 <li>Work on the task.</li>
-                <li>End work when the timer rings and put a checkmark on a piece of paper.</li>
-                <li>If you have fewer than four checkmarks, take a short break (3-5 minutes), then go to step 2.</li>
-                <li>After 4 pomodoros, take a long break (15-30 minutes), reset your checkmark count to zero, then go to step 1.</li>
+                <li>End work when the timer rings and put a checkmark on your task in the <NavLink to="/todolist">To Do List</NavLink>.</li>
+                <li>If you have fewer than four tasks checkmarked, take a short break (3-5 minutes), then go to step 1.</li>
+                <li>After completing 4 pomodoros, take a long break (15-30 minutes), "Clear All" your tasks, then go to step 1.</li>
             </ol>
         </div>
 );
